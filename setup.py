ebba5ccb0f7ed0e7f2029ae8920910b9c5737cdf
Daniel Frees <danielfrees@g.ucla.edu>
2023-06-23 17:03:33 -0700

    v4 build & metadata

--- setup.py
+++ setup.py
@@ -1,14 +1,8 @@
 from skbuild import setup
 
 setup(
-    name = "racplusplus",
-    version = "0.0.1",
     python_requires = ">=3.11",
-    description = "Reciprocal Agglomerative Clustering, optimized for speed/ parallelization in C++.",
-    author = "Porter Hunley, Daniel Frees",
-    author_email = "porterhunley@themedicalboard.net, danielfrees@g.ucla.edu",
     packages = ["racplusplus"],
     package_dir={"": "src"},
-    cmake_install_dir="src/racplusplus",
-    license = "MIT"
+    cmake_install_dir="src/racplusplus"
 )
