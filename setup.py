14359ed7f77552c602d0664756d48d9a1d63c727
Porter Hunley <porterehunley@gmail.com>
2023-07-12 09:10:24 -0700

    Switch build system

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(
     python_requires = ">=3.11",
     packages = ["racplusplus"],
     package_dir={"": "src"},
+    install_requires = ["pybind11"],
     cmake_install_dir="src/racplusplus"
 )
