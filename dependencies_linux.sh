56980428315c163d5515ce7135269280f43ebe07
Porter Hunley <porterehunley@gmail.com>
2023-07-12 16:08:46 -0700

    Remove some package installs

--- dependencies_linux.sh
+++ dependencies_linux.sh
@@ -3,18 +3,6 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# Update the package list
-apt-get update
-
-# Install g++ and clang if they are not already installed
-apt-get install -y g++ clang
-
-# Install libomp-dev (for OpenMP)
-apt-get install -y libomp-dev
-
-# Install unzip if it's not already installed
-apt-get install -y unzip
-
 # Download Eigen
 wget https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.zip
 
@@ -24,18 +12,9 @@ unzip eigen-3.4.0.zip
 # Move Eigen headers to /usr/local/include to make them available system-wide
 cp -r eigen-3.4.0/Eigen /usr/local/include/
 
-# Install pip if it's not already installed
-if ! command -v pip &> /dev/null
-then
-    apt-get install -y python3-pip
-fi
-
-# Install pybind11
-pip install pybind11
-
 # Set CC and CXX environment variables to clang
-export CC=$(which clang)
-export CXX=$(which clang++)
+export CC=$(which gcc)
+export CXX=$(which g++)
 export LOCAL_INCLUDE_DIRS=/usr/local/include
 
 # Print the environment variables
