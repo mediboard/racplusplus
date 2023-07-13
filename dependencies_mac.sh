883723bb73737c2450c3bb6ca13d5910cc4cd98d
Porter Hunley <porterehunley@gmail.com>
2023-07-13 14:30:47 -0700

    delete directory if present

--- dependencies_mac.sh
+++ dependencies_mac.sh
@@ -3,13 +3,17 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-OMP_BUILD_DIR="openmp-12.0.1.src/build_${CIBW_BUILDING_IDENTIFIER}"
+OMP_BUILD_DIR="openmp-12.0.1.src/build"
 
 # Download and unpack OpenMP source code
 curl -OL https://github.com/llvm/llvm-project/releases/download/llvmorg-12.0.1/openmp-12.0.1.src.tar.xz
 tar xvf openmp-12.0.1.src.tar.xz
 
 # Create build directory and navigate to it
+if [ -d "${OMP_BUILD_DIR}" ]; then
+  echo "Directory ${OMP_BUILD_DIR} exists. Removing..."
+  rm -rf ${OMP_BUILD_DIR}
+fi
 mkdir ${OMP_BUILD_DIR}
 cd ${OMP_BUILD_DIR}
 
@@ -29,7 +33,11 @@ curl -OL https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.zip
 # Unzip Eigen
 unzip eigen-3.4.0.zip
 
-EIGEN_BUILD_DIR="eigen-3.4.0/build_${CIBW_BUILDING_IDENTIFIER}"
+EIGEN_BUILD_DIR="eigen-3.4.0/build"
+if [ -d "${EIGEN_BUILD_DIR}" ]; then
+  echo "Directory ${EIGEN_BUILD_DIR} exists. Removing..."
+  rm -rf ${EIGEN_BUILD_DIR}
+fi
 
 # Create build directory
 mkdir ${EIGEN_BUILD_DIR} 
