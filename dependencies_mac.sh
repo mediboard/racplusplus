39a9c2c6a02641fb4b61a7844cf470b67de515b7
Porter Hunley <porterehunley@gmail.com>
2023-07-13 14:16:39 -0700

    Make builds different from arch

--- dependencies_mac.sh
+++ dependencies_mac.sh
@@ -3,13 +3,25 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+BUILD_IDENTIFIER=${CIBW_BUILD}
+if [[ $BUILD_IDENTIFIER == *"_x86_64"* ]]; then
+  ARCH="x86_64"
+elif [[ $BUILD_IDENTIFIER == *"_arm64"* ]]; then
+  ARCH="arm64"
+else
+  echo "Unknown architecture"
+  exit 1
+fi
+
+OMP_BUILD_DIR="openmp-12.0.1.src/build_${ARCH}"
+
 # Download and unpack OpenMP source code
 curl -OL https://github.com/llvm/llvm-project/releases/download/llvmorg-12.0.1/openmp-12.0.1.src.tar.xz
 tar xvf openmp-12.0.1.src.tar.xz
 
 # Create build directory and navigate to it
-mkdir openmp-12.0.1.src/build
-cd openmp-12.0.1.src/build
+mkdir ${OMP_BUILD_DIR}
+cd ${OMP_BUILD_DIR}
 
 # Configure and build OpenMP
 cmake ..
@@ -27,9 +39,11 @@ curl -OL https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.zip
 # Unzip Eigen
 unzip eigen-3.4.0.zip
 
+EIGEN_BUILD_DIR="eigen-3.4.0/build_${ARCH}"
+
 # Create build directory
-mkdir eigen-3.4.0/build
-cd eigen-3.4.0/build
+mkdir ${EIGEN_BUILD_DIR} 
+cd ${EIGEN_BUILD_DIR} 
 
 # Configure
 cmake ..
