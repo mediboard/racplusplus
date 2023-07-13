0ecabc8fa3ea21134a5393fbd2537079cf41ce0c
Porter Hunley <porterehunley@gmail.com>
2023-07-13 09:48:33 -0700

    Remove pybind11 from setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,5 @@ setup(
     python_requires = ">=3.11",
     packages = ["racplusplus"],
     package_dir={"": "src"},
-    install_requires = ["pybind11"],
     cmake_install_dir="src/racplusplus"
 )
