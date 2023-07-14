6c1dc43eab1ab32e9ca87a4ebe6b9a964530c06c
Porter Hunley <porterehunley@gmail.com>
2023-07-14 14:31:43 -0700

    Bump python in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from skbuild import setup
 
 setup(
-    python_requires = ">=3.11",
+    python_requires = ">=3.7",
     packages = ["racplusplus"],
     package_dir={"": "src"},
     cmake_install_dir="src/racplusplus"
